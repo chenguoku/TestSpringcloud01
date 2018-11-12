fe4b2eb962c0d7e7a8354e6b4b3cd4a175b7f6e1
陈国库 <cgkhh1996@163.com>
2018-11-13 00:13:21 +0800

    2018/11/13 springcloudtest01 创建了 生产者

---
+++ service-simple-consumer-user/src/main/java/com/hh/cloud/servicesimpleconsumeruser/ServiceSimpleConsumerUserApplication.java
@@ -0,0 +1,12 @@
+package com.hh.cloud.servicesimpleconsumeruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceSimpleConsumerUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceSimpleConsumerUserApplication.class, args);
+	}
+}
---
+++ service-simple-provider-user/src/main/java/com/hh/cloud/servicesimpleprovideruser/ServiceSimpleProviderUserApplication.java
@@ -0,0 +1,12 @@
+package com.hh.cloud.servicesimpleprovideruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceSimpleProviderUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceSimpleProviderUserApplication.class, args);
+	}
+}
