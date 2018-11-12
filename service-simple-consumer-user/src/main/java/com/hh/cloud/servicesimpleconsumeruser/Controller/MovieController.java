eb900ef3b85e1e2f0db06a3832c5e0a4be79ae09
陈国库 <cgkhh1996@163.com>
2018-11-13 01:17:52 +0800

    都可以运行的生产者 和 消费者

---
+++ service-simple-consumer-user/src/main/java/com/hh/cloud/servicesimpleconsumeruser/Controller/MovieController.java
@@ -0,0 +1,23 @@
+package com.hh.cloud.servicesimpleconsumeruser.Controller;
+
+import com.hh.cloud.servicesimpleconsumeruser.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class MovieController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/movie/{id}")
+    public User findById(@PathVariable Long id){
+        ResponseEntity<User> userResponseEntity = restTemplate.getForEntity("http://localhost:7900/simple/" + id, User.class);
+        return userResponseEntity.getBody();
+    }
+
+}
