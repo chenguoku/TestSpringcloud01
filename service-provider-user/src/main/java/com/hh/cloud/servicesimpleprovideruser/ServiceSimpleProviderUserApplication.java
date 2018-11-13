b155dc2e4a73105ce5b6a3f6df8a344aa099f639
陈国库 <cgkhh1996@163.com>
2018-11-14 01:19:09 +0800

    将微服务注册到Eureka上，并进行安全认证

--- service-provider-user/src/main/java/com/hh/cloud/servicesimpleprovideruser/ServiceSimpleProviderUserApplication.java
+++ service-provider-user/src/main/java/com/hh/cloud/servicesimpleprovideruser/ServiceSimpleProviderUserApplication.java
@@ -2,8 +2,10 @@ package com.hh.cloud.servicesimpleprovideruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ServiceSimpleProviderUserApplication {
 
 	public static void main(String[] args) {
