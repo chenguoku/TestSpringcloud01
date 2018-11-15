bc178b2b6ec585e0557f1227d998876459a22097
陈国库 <cgkhh1996@163.com>
2018-11-16 01:06:53 +0800

    Ribbon进行了负载均衡，并且通过配置文件的方式进行了 自定义 负载均衡方式 Ribbon中不使用Eureka

---
+++ service-consumer-user-ribbon-properties-customizing/src/main/java/com/hh/cloud/servicesimpleconsumeruser/Controller/MovieController.java
@@ -0,0 +1,44 @@
+package com.hh.cloud.servicesimpleconsumeruser.Controller;
+
+import com.hh.cloud.servicesimpleconsumeruser.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class MovieController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @GetMapping("/movie/{id}")
+    public User findById(@PathVariable Long id){
+
+        ResponseEntity<User> userResponseEntity = restTemplate.getForEntity("http://service-provider-user/simple/" + id, User.class);
+
+        ServiceInstance choose1 = loadBalancerClient.choose("service-provider-user");
+        System.out.println("---------------  :  "+choose1.getHost()+" : "+choose1.getPort()+" : " + choose1.getServiceId());
+        return userResponseEntity.getBody();
+    }
+
+    @GetMapping("/test")
+    public String test(){
+
+        ServiceInstance choose1 = loadBalancerClient.choose("service-provider-user");
+        System.out.println("1111  :  "+choose1.getHost()+" : "+choose1.getPort()+" : " + choose1.getServiceId());
+
+        ServiceInstance choose2 = loadBalancerClient.choose("service-provider-user2");
+        System.out.println("2222  :  "+choose2.getHost()+" : "+choose2.getPort()+" : " + choose2.getServiceId());
+        return "1";
+    }
+
+}
