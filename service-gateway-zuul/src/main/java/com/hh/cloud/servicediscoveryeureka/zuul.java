c93014d865451f08323169ece20ba018e4fcc368
陈国库 <cgkhh1996@163.com>
2018-11-30 00:49:54 +0800

    zuul的基本使用

--- service-gateway-zuul/src/main/java/com/hh/cloud/servicediscoveryeureka/zuul.java
+++ service-gateway-zuul/src/main/java/com/hh/cloud/servicediscoveryeureka/zuul.java
@@ -2,13 +2,13 @@ package com.hh.cloud.servicediscoveryeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class ServiceDiscoveryEurekaApplication {
+@EnableZuulProxy
+public class zuul {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceDiscoveryEurekaApplication.class, args);
+        SpringApplication.run(zuul.class, args);
     }
 }
