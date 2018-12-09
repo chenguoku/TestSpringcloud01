0c567ad4f68da26d10aba479b1d09c4c33b7b75e
陈国库 <cgkhh1996@163.com>
2018-12-09 21:47:40 +0800

    config-server的使用自动刷新

---
+++ config-learn/service-config-client-refresh-bus/src/main/java/com/hh/cloud/servicediscoveryeureka/ConfigClientRefreshBus.java
@@ -0,0 +1,12 @@
+package com.hh.cloud.servicediscoveryeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientRefreshBus {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientRefreshBus.class, args);
+    }
+}
